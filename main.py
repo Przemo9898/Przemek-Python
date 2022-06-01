484b3d205143799f502585e1c5d45c096d8a6071
Przemo9898 <97949026+Przemo9898@users.noreply.github.com>
2022-06-01 07:20:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,120 +1,106 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from  PIL import  Image
+from math import *
 
-# plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro-')
-# plt.axis([0, 6, 0, 20])
-# plt.show()
+#zestaw A
+#zad1
+# def zad1(slownik_arg):
+#     lista = []
+#     for key in slownik_arg.keys():
+#         if type(slownik_arg[key]) == int:
+#             lista.append(key)
+#     return lista
 #
-# plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'r')
-# plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'o')
 #
-# plt.axis([0, 6, 0, 20])
-# plt.show()
+# slownik = {1: 2, "a": 3, 4.5: 1, 2: "a"}
+# print(zad1(slownik))
+#
+# #zad2
+# plik = open('tekst.txt', 'r', encoding='utf-8')
+# plik.seek(24)
+# znaki = plik.read(20)
+# plik.close()
+# duze_znaki = 0
+# for znak in znaki:
+#     if znak.isupper():
+#         duze_znaki += 1
+# if duze_znaki > 0:
+#     print('Ilość dużych liter: ' + str(duze_znaki))
+# else:
+#     print('brak dużych liter')
+#
+# #zad3
+# lista_a = [2, 3, 5, 1, 6, 8, 3, 6, 8, 1, 9, 2]
+# lista_b = [lista_a[i] for i in range(0, len(lista_a), 2)]
+# print(lista_a)
+# print(lista_b)
+#
+# #zad4
+# a = pow(pi, 3) + pow(log(64, 2) + sin(45), 1/4)
+# print(round(a, 2))
+#
+# #zad5
+# try:
+#     a = int(input('Wczytaj liczbę a: '))
+#     b = int(input('Wczytaj liczbę b: '))
+#     wynik = a**b
+#     plik = open('zadanie5.txt', 'w')
+#     plik.write(str(wynik))
+#     plik.close()
+# except ValueError:
+#     print('nie wczytano liczby całkowitej')
 
-# t = np.arange(0., 5., 0.2)
-# plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
-# plt.legend(labels=['liniowa', 'kwadratowa', 'szescienna'])
-# plt.show()
 
-# x = np.linspace(0, 2, 100)
-# plt.plot(x, x, label="liniowa")
-# plt.plot(x, x**2, label="kwadratowa")
-# plt.plot(x, x**3, label="sześcienna")
+#zestawB
+#zad1
+# plik = open('tekst.txt', 'r', encoding='utf-8')
+# plik.seek(63)
+# znaki = plik.read(42)
+# plik.close()
+# male_znaki = 0
+# for znak in znaki:
+#     if znak.islower():
+#         male_znaki += 1
+# if male_znaki > 0:
+#     print('Ilość małych liter: ' + str(male_znaki))
+# else:
+#     print('brak małych liter')
 #
-# plt.xlabel('etykieta x')
-# plt.ylabel("etykieta y")
-# plt.title("Prosty wykres")
-# plt.legend()
-# plt.savefig('wykres matplot.png')
+# #zad2
+# def zad2(lista_arg):
+#     lista_wynikowa = []
+#     for element in lista_arg:
+#         if (type(element) == int) | (type(element) == float):
+#             lista_wynikowa.append(element)
 #
-# plt.show()
-# im1 = Image.open('wykres matplot.png')
-# im1 = im1.convert('RGB')
-# im1.save('nowy.jpg')
+#     return lista_wynikowa, max(lista_wynikowa), min(lista_wynikowa)
+#
+#
+# lista = [2, 3.5, 24, 'abc', 'a', -1]
+# print(zad2(lista))
 
-# x = np.arange(0, 10, 0.1)
-# s = np.sin(x)
-# plt.plot(x, s, label="sin(x)")
-# plt.xlabel('x')
-# plt.ylabel('sin(x)')
-# plt.title('Wykres sin(x)')
-# plt.legend()
-# plt.show()
+#zad3
+# try:
+#     a = int(input('Podaj liczbę a: '))
+#     b = int(input('Podaj liczbę b: '))
+#     wynik = pow(sin(a), 2) + pow(cos(b), 2)
+#     plik = open('zadanie3.txt', 'w')
+#     if wynik == 1:
+#         plik.write('jedynka trygonometryczna zachodzi')
+#     else:
+#         plik.write('nie zachodzi jedynka trygonometryczna')
+#     plik.close()
+# except ValueError:
+#     print("nie wczytano liczby całkowitej")
 
-# data = {'a': np.arange(50),
-#         'c': np.random.randint(0, 50, 50),
-#         'd': np.random.randn(50)}
-# data['b'] = data['a'] + 10 * np.random.randn(50)
-# data['d'] = np.abs(data['d']) * 100
-# print(f"a={data['a'][0]}, b={data['b'][0]}, c={data['c'][0]}, d={data['d'][0]}")
-# plt.scatter('a', 'b', c='c', s='d', data=data)
-# plt.xlabel('wartość a')
-# plt.ylabel('wartość b')
-# plt.show()
+#zad4
+# lista_a = [14, 21, 3, 16, 2, 5, 1, 10]
+# lista_b = [a for a in lista_a if a > lista_a[-1]]
+# print(lista_a)
+# print(lista_b)
+#
+# #zad5
+# b = exp(3) + pow(log(pow(cos(36),2) + pi), 1/5)
+# print(round(b, 2))
 
-x1 = np.arange(0, 2, 0.02)
-x2 = np.arange(0, 2, 0.02)
 
-y1 = np.sin(2 * np.pi * x1)
-y2 = np.cos(2 * np.pi * x2)
 
-# plt.subplot(2, 1, 1,)
-# plt.plot(x1, y1, '-')
-# plt.title('wykres sin(x)')
-# plt.xlabel('x')
-# plt.ylabel('sin(x)')
-# ax = plt.subplot(2, 1, 2)
-# plt.plot(x2, y2, 'r-')
-#
-# plt.xlabel('x')
-# plt.ylabel('cos(x)')
-# plt.title('wykres cos(x)')
-# plt.subplots_adjust(hspace=0.5)
-# plt.show()
 
-# x1 = np.arange(0, 2.0, 0.02)
-# x2 = np.arange(0, 2.0, 0.02)
-#
-# y1 = np.sin(2 * np.pi * x1)
-# y2 = np.cos(2 * np.pi * x2)
-#
-# fig, axs = plt.subplots(3, 2, )
-# axs[0, 0].plot(x1, y1, '-')
-# axs[0, 0].set_title('wykres sin(x)')
-# axs[0, 0].set_xlabel('x')
-# axs[0, 0].set_ylabel('sin(x)')
-#
-# axs[1, 1].plot(x2, y2, 'r-')
-# axs[1, 1].set_title('wykres cos(x)')
-# axs[1, 1].set_xlabel('x')
-# axs[1, 1].set_ylabel('cos(x)')
-#
-# axs[2, 0].plot(x2, y2, 'r-')
-# axs[2, 0].set_title('wykres cos(x)')
-# axs[2, 0].set_xlabel('x')
-# axs[2, 0].set_ylabel('cos(x)')
-#
-# fig.delaxes(axs[0, 1])
-# fig.delaxes(axs[1, 0])
-# fig.delaxes(axs[2, 1])
-# plt.show()
-
-# x = np.random.randn(10000)
-# plt.hist(x, bins=50, facecolor='g', alpha=0.75, density=True)
-#
-# plt.xlabel("Wartości")
-# plt.ylabel('Prawdopodobieństwo')
-# plt.title('Histogram')
-# plt.grid()
-# plt.show()
-
-df = pd.read_csv('dane.csv', header=0, sep=";", decimal=".")
-print(df)
-seria = df.groupby('Imię i nazwisko')['Wartość zamówienia'].sum()
-wedges, texts, autotext = plt.pie(x=seria, labels=seria.index, autopct=lambda pct: "{:.lf}%".format(pct), textprops=dict(color="black"), colors=['red', 'green'])
-plt.title('Suma zamówień dla sprzedawców')
-plt.legend(loc='lower right')
-plt.ylabel('Procentowy wynik wartości zamówienia')
-plt.show()
